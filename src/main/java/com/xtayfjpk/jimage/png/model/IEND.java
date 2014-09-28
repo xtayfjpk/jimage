bc4889cd6f6c42783aeece0d83a98368b6ffb18e
zhangjin <zhangjin@kingyea.com.cn>
2014-09-28 22:13:43 +0800

    添加初始参数设置

--- src/main/java/com/xtayfjpk/jimage/png/model/IEND.java
+++ src/main/java/com/xtayfjpk/jimage/png/model/IEND.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class IEND extends Chunk {
+	
+	public IEND() {
+		this.setChunkType(ChunkType.IEND);
+		this.setMultiple(false);
+		this.setCritical(true);
+	}
 
 	@Override
 	public int doRead(InputStream input) throws IOException {
